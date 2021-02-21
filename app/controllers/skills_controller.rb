4b7a7bcf926e7668599c3daefd19d4a7c1086642
akkun08 <akihiroaika1112@gmail.com>
2021-02-21 16:43:33 +0900

    投稿編集機能の実装

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,6 @@
 class SkillsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
+  before_action :find_params, only: [:show, :edit, :update]
 
   def index
     @skills = Skill.order("created_at DESC")
@@ -19,10 +20,24 @@ class SkillsController < ApplicationController
   end
 
   def show
-    @skill = Skill.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @skill.update(skill_params)
+      redirect_to skill_path
+    else
+      render :edit
+    end
   end
 
   private
+
+  def find_params
+    @skill = Skill.find(params[:id])
+  end
 
   def skill_params
     params.require(:skill).permit(:image, :name, :category_id, :formulation_text, :recipe_text).merge(user_id: current_user.id)
